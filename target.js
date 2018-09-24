2c5a299d3eeff2ec22cfd5d1313923ba3783f265
Morgan Mora Martínez <morganmora@gmail.com>
2018-09-25 00:49:38 +0200

    Adding OS selection

--- target.js
+++ target.js
@@ -2,10 +2,15 @@ var path = require('path');
 var fs = require('fs');
 const config = require('./package');
 const http = require("http");
+var os = require("os");
 var extract = require('extract-zip');
 
 let defaultUrl = `https://github.com/lequal/i-CodeCNES/releases/download/${config.icodeVersion}/i-CodeCNES-${config.icodeVersion}-CLI-linux.gtk.x86_64.zip`;
-let fileZipName = path.resolve(__dirname, 'src', `i-CodeCNES-${config.icodeVersion}-CLI-linux.gtk.x86_64.zip`);
+let defaultFileName = path.resolve(__dirname, 'src', `i-CodeCNES-${config.icodeVersion}-CLI-linux.gtk.x86_64.zip`);
+let windowsJava64b = path.resolve(__dirname, 'src', `i-CodeCNES-${config.icodeVersion}-CLI-win32.win32.x86_64.zip`);
+let windowsJava32b = path.resolve(__dirname, 'src', `i-CodeCNES-${config.icodeVersion}-CLI-win32.win32.x86.zip`);
+let linuxJava32b = path.resolve(__dirname, 'src', `i-CodeCNES-${config.icodeVersion}-CLI-linux.gtk.x86.zip`);
+let mcOsJava64b = path.resolve(__dirname, 'src', `i-CodeCNES-${config.icodeVersion}-CLI-macosx.cocoa.x86_64.zip`);
 let target = exports;
 target.home = process.env.HOME || process.env.USERPROFILE;
 target.home += '/.icode';
@@ -32,8 +37,18 @@ target.init = function() {
 }
 
 function createFolder() {
-  file = fs.createReadStream(fileZipName);
-  extract(fileZipName, {dir: target.home}, function (err) {});
+  switch (os.platform()){
+    case 'win32':
+    
+    break;
+
+    case 'darwin':
+
+    default:
+
+  }
+  file = fs.createReadStream(defaultFileName);
+  extract(defaultFileName, {dir: target.home}, function (err) {});
 }
 
 target.init();
